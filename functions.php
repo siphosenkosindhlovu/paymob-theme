e25d619bc7a68b8dcc9d19dce0bb4cb61a72e91b
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-05-06 10:30:37 +0200

    Added subscribe form

--- functions.php
+++ functions.php
@@ -409,6 +409,7 @@ add_action('widgets_init', 'paymob_widgets_init');
  */
 function paymob_scripts()
 {
+	wp_enqueue_style('mailchimp', '//cdn-images.mailchimp.com/embedcode/classic-10_7.css', array(), PAYMOB_VERSION);
 	wp_enqueue_style('paymob-style', get_stylesheet_uri(), array(), PAYMOB_VERSION);
 	wp_enqueue_style('paymob-main', get_template_directory_uri() . '/dist/css/style.css', array(), PAYMOB_VERSION);
 	wp_style_add_data('paymob-style', 'rtl', 'replace');
